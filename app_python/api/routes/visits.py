e8634f7f08d7db1131ec7d857c14e6d23f3a8a09
Ruslan Israfilov <notverywellf@gmail.com>
2021-10-04 22:40:24 +0300

    lab 13:  fix visits

--- app_python/api/routes/visits.py
+++ app_python/api/routes/visits.py
@@ -8,7 +8,7 @@ router = APIRouter(tags=["visits"])
 @router.get("/visits")
 async def get_visits() -> str:
     try:
-        async with aiofiles.open('visits.txt', mode='r') as f:
+        async with aiofiles.open('app_python/visits/visits.txt', mode='r') as f:
             visits = await f.read()
         return visits
     except Exception as e:
@@ -18,5 +18,5 @@ async def get_visits() -> str:
 
 @router.get("/")
 async def visit(req: Request) -> None:
-    async with aiofiles.open("visits.txt", mode="a") as f:
+    async with aiofiles.open("app_python/visits/visits.txt", mode="a") as f:
         await f.write(f"Client: {req.client.host} visited site at {datetime.datetime.now()}\n")
